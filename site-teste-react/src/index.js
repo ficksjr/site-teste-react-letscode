bb0619a6f2de719afb1d2bbac7a2228633fbbe75
ficksjr <ficksjr@gmail.com>
2021-07-09 15:17:04 -0300

    Commit no formulário

--- site-teste-react/src/index.js
+++ site-teste-react/src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import App2 from './components/App2.js';
 import App3 from './components/App3.js';
-import {Lista,Item} from './components/Lista.js';
+import Formulario from './components/Formulario.js';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Lista>
-      Texto 1
-      <Item key={999} id={999} completo={false}>Teste1</Item>
-      <Item key={998} id={998} completo={true}>Teste2</Item>
-    </Lista>
+    <Formulario/>
   </React.StrictMode>,
   document.getElementById('root')
 );
