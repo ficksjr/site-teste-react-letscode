a765b4270a2d9b84463e88f3b4c5eae0ed04e9c8
ficksjr <ficksjr@gmail.com>
2021-07-08 23:30:46 -0300

    lista app adicionado

--- site-teste-react/src/index.js
+++ site-teste-react/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import App2 from './components/App2.js';
 import App3 from './components/App3.js';
-import App5 from './components/App5.js';
+import Lista from './components/Lista.js';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App5/>
+    <Lista/>
   </React.StrictMode>,
   document.getElementById('root')
 );
