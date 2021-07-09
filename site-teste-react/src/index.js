7962e4b615025a35f5f9a535a0669150ec5841f4
ficksjr <ficksjr@gmail.com>
2021-07-09 14:44:35 -0300

    Lista final commit

--- site-teste-react/src/index.js
+++ site-teste-react/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import App2 from './components/App2.js';
 import App3 from './components/App3.js';
-import Lista from './components/Lista.js';
+import {Lista,Item} from './components/Lista.js';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Lista/>
+    <Lista>
+      Texto 1
+      <Item key={999} id={999} completo={false}>Teste1</Item>
+      <Item key={998} id={998} completo={true}>Teste2</Item>
+    </Lista>
   </React.StrictMode>,
   document.getElementById('root')
 );
