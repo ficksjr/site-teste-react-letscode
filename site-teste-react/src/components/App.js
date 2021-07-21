c8f20bfc0f406d7b261229582fb8a2cb9adc9641
Paulo Renato <ficksjr@gmail.com>
2021-07-20 23:44:06 -0300

    Update App.js

--- site-teste-react/src/components/App.js
+++ site-teste-react/src/components/App.js
@@ -1,30 +1,36 @@
 import React from "react";
 
+const api = ('1', '2', '3', '4', '5', '6');
+const getListaCompras = () => ([api]) 
+
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      console.log("constructor");
-    }
-    
-    componentDidMount() {
-      console.log("componentDidMount");
-    }
-    
-    componentWillUnmount() {
-      console.log("componentWillUnmount");
-    }
-    
-    render() {
-      console.log("render");
-    
-      return (
-        <ul>
-          <li><a>Home</a></li>
-          <li><a>Sobre</a></li>
-          <li><a>Contato</a></li>
-        </ul>
-      )
+  constructor(props) {
+    super(props);
+    this.state = {
+      listaCompras: []
     }
   }
+  
+  componentDidMount() {
+  /*
+   * Vamos supor que api é global e
+   * o método abaixo retorna a lista de
+   * compras em forma de array
+   */
+    const lista = api.getListaCompras(); 
+    this.setState({ lista });
+  }
+  
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.listaCompras.map(compra => <li>{compra.nome}</li>)}
+        </ul>
+      </div>
+    )
+    
+  }
+}
 
 export default App
